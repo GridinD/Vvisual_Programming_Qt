4a249f53528fda337f0b4495fa6e0ba4a6642ad8
GridinD <gridin.danila@yandex.ru>
2023-12-20 01:33:42 +0700

    refactoring

---
+++ project/formuly.h
@@ -0,0 +1,9 @@
+#ifndef FORMULY_H
+#define FORMULY_H
+#include <cmath>
+
+double formula(double f, double d);
+float calculate(int x1,int x2,int y1,int y2);
+float pix_translate(float x, int pix_distance);
+
+#endif // FORMULY_H
--- project/mainwindow.h
+++ project/mainwindow.h
@@ -1,11 +1,21 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
 #include <QMainWindow>
+#include <QtWidgets>
+#define SIZE_MAP_X 1500
+#define SIZE_MAP_Y 1500
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+
+enum class type_material : int{
+    NO_MATERIAL = 0,
+    GLASS,
+    IRR_GLASS,
+    CONCRETE,
+    WOOD,
+    DRYWALL,
+    COUNT_MATERIAL
+};
+
 
 class MainWindow : public QMainWindow
 {
@@ -14,8 +24,15 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
+    int **map_obj;
 private:
-    Ui::MainWindow *ui;
+    QLabel *label_;
+    QGroupBox *gradientGroupBox_;
+    void drawSignalStrength(QPixmap *map);
+    void setupMap();
+    void createColorMap();
+
+
 };
+void add_material_to_map(int **map, int px, int py, int sx, int sy, int type);
 #endif // MAINWINDOW_H
